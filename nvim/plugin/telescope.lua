1272bdb92b9799efc2ea7731114349e4bff76188
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2026-09-05 21:24:02 +0800

    fix(find_files): Update find_files keybind

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -3,4 +3,4 @@ local map = vim.keymap.set
 local telescope = require("telescope.builtin")
 map("n", "<leader>f", telescope.fd, { desc = "Find files" })
 map("n", "<leader>o", telescope.oldfiles, { desc = "Old files" })
-map("n", "<leader>m", telescope.fd, { desc = "Live grep" })
+map("n", "<leader>l", telescope.live_grep, { desc = "Live grep" })
