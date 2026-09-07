ddfb5125821a923378e2c4c09cd92235154b357b
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2026-09-07 13:53:36 +0800

    feat(python): Add Python ftplugin script runner

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,5 +1,45 @@
-vim.keymap.set("n", "<F5>", function()
-  local args = vim.fn.input("CLI Args: ")
-  vim.cmd("vsplit | term python % " .. args)
-  vim.cmd("stopinsert")
-end, { buffer = true, desc = "Run Python script in NeoVim" })
+local map = vim.keymap.set
+local cmd = vim.cmd
+
+map("n", "<F5>", function()
+  local run_options = {
+    {
+      label = "Run current Python file",
+      action = function()
+        return "uv run python %"
+      end,
+    },
+    {
+      label = "Run Python script in NeoVim",
+      action = function()
+        local filepath = vim.fn.expand("%"):gsub("\\", "/")
+        return "uv run python " .. filepath
+      end,
+    },
+    {
+      label = "Run Python module in NeoVim",
+      action = function()
+        local module_name = vim.fn.input("Module: ")
+        return "PYTHONPATH=src uv run python -m " .. module_name
+      end,
+    },
+  }
+  vim.ui.select(run_options, {
+    prompt = "Select Run Mode:",
+    format_item = function(item)
+      return item.label
+    end,
+  }, function(choice)
+    if not choice then
+      return
+    end
+
+    cmd("vsplit | enew")
+
+    local args = vim.fn.input("args: ")
+
+    vim.fn.termopen({ "bash", "-c", choice.action() .. " " .. args })
+
+    cmd("stopinsert")
+  end)
+end, { buffer = true, desc = "Python Runner" })
