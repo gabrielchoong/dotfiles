0a4706cdf4f01412cfd7491f2f5d76b5b22b6102
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2026-09-07 13:44:48 +0800

    feat(conform): Formatter configuration

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -1,9 +1,31 @@
 require("conform").setup({
+  formatters = {
+    oxfmt = {
+      command = "npx.cmd",
+      prepend_args = { "--no-install", "oxfmt" },
+    },
+    shfmt = {
+      command = "shfmt.exe",
+      prepend_args = { "-i", "2", "-ci", "-s" },
+    },
+  },
   format_on_save = {
-    timeout_ms = 500,
+    timeout_ms = 700,
     lsp_format = "fallback",
   },
   formatters_by_ft = {
     lua = { "stylua" },
+    python = { "ruff_format" },
+    javascript = { "oxfmt" },
+    typescript = { "oxfmt" },
+    javascriptreact = { "oxfmt" },
+    typescriptreact = { "oxfmt" },
+    json = { "oxfmt" },
+    jsonc = { "oxfmt" },
+    html = { "oxfmt" },
+    css = { "oxfmt" },
+    sh = { "shfmt" },
+    bash = { "shfmt" },
+    markdown = { "prettier" },
   },
 })
