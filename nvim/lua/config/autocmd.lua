488f4223a6ede0984c882f71df08b17a593bd17c
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2026-09-07 13:47:48 +0800

    refactor: Group autocmd

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
 vim.api.nvim_create_autocmd("UIEnter", {
   callback = function()
     vim.o.clipboard = "unnamedplus"
@@ -17,9 +24,5 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   desc = "Disable undofile for certain files",
 })
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  callback = function()
-    vim.hl.on_yank()
   end,
 })
